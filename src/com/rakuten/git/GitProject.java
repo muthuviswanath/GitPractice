67ea5fc48bb8395874185064656b5a0fb7b4d693
muthuviswanath <muthuviswanath@outlook.com>
2023-06-13 10:17:36 +0530

    third

--- src/com/rakuten/git/GitProject.java
+++ src/com/rakuten/git/GitProject.java
@@ -5,6 +5,22 @@ public class GitProject {
 	private int age;
 	private String name;
 	
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
 	public GitProject(int age, String name) {
 		this.age = age;
 		this.name = name;
